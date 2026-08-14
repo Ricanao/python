5cd2eb6b439115c610f4069c35e5f6d5c3ff76ce
Ricardo Montanini <ricardomontaninijunior@gmail.com>
2026-08-13 22:33:21 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,14 @@
+
+# 1. Operações Aritméticas
+print("--- Operações Aritméticas ---")
+a = 6
+b = 7
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")
+print(f"{a} % {b} = {a % b}")
+print(f"{a} ** {b} = {a ** b}")
+print("-" * 20)
